16d6239d667044cf32c75eb00549b537c670e0ee
Nils Pettersson <nils-pettsson@outlook.com>
2020-08-25 11:14:17 +0200

    ray marching testing

--- shaders/post.fs
+++ shaders/post.fs
@@ -17,6 +17,26 @@ in vec3 toCamera;
 in vec4 worldPosition;
 
 
+float getDist(vec3 point){
+	vec4 ball = vec4(0, 1, 6, 1);
+	float ballDist = length(point - ball.xyz) - ball.w;
+	return ballDist;
+}
+
+float rayMarch(vec3 rayOrigin, vec3 rayDir){
+	float DistOrigin = 0;
+	
+	for(int i = 0; i < 100; i++){
+		vec3 point = rayOrigin + rayDir * DistOrigin;
+		float dist = getDist(point);
+		DistOrigin += dist;
+		if(DistOrigin > 1000 || dist < 0.01 ){
+			break;
+		}
+	}
+	return DistOrigin;
+}
+
 
 void main(){
 	
@@ -31,12 +51,21 @@ void main(){
 	z *= 0.01;
 	
 	
-
+	vec2 res = vec2(1, 1);
+	vec2 uv = (tex_coords - 0.5 * res.xy) / res.y;
 	
-	//texture.xyz += z;
+	vec3 rayOrigin = vec3(0, 1, 0);
+	vec3 rayDir = normalize(vec3(uv.x, uv.y, 1));
 	
+	float dis = rayMarch(rayOrigin, rayDir);
 	
-	gl_FragColor = texture;
+	dis /= 22;
+	if(dis >= 1){
+		gl_FragColor = texture;
+	}
+	else{
+		gl_FragColor = vec4(dis, dis, dis,1);
+	}
 	
 	
 	

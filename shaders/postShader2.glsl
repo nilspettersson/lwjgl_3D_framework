7ec61f9923f50d28acd3964a9b3da08ddb5941e2
Nils Pettersson <nils-pettsson@outlook.com>
2020-11-27 20:57:36 +0100

    added image

--- shaders/postShader2.glsl
+++ shaders/postShader2.glsl
@@ -1,31 +1,28 @@
 #include lib/rayMarching.glsl;
+#include lib/sdf.glsl;
 
 uniforms{
 	float time
 }
 
 
-float sdBox( vec3 p, vec3 b )
-{
-  vec3 q = abs(p) - b;
-  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
-}
-
 
 float sdf(vec3 point){
 	vec4 ball = vec4(4, -7, 2, 1);
 	float ballDist = length(point - ball.xyz) - ball.w;
 
-	vec3 transform1 = vec3(0, -7.5, -2);
+	vec3 transform1 = vec3(4, -7.5, 2);
 	float box = sdBox(point - transform1, vec3(0.5, 0.5, 0.5));
+	transform1 = vec3(4, -8, 2);
+	float box2 = sdBox(point - transform1, vec3(2, 0.2, 2));
 
-    return ballDist;
+    return min(box, box2);
 }
 
 fragment{
 	Ray rayDir = getRay();
 	rayDir = rayMarch(rayDir);
-	vec4 diffuse = rayMarchDiffuse(rayDir, vec3(1));
+	vec4 diffuse = rayMarchDiffuse(rayDir, vec3(1, 0.5, 0.5));
 
 
 	vec4 output = texture;
